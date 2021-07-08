4eac062f33f000e93001bfbbc16e091b4f8c51ca
Liann Yim <hello@liannyim.co>
2021-07-09 09:08:58 +1200

    Minor tidyups.

--- assets/application.js
+++ assets/application.js
@@ -1,29 +1,29 @@
 // Put your application javascript here
 $(document).ready(function () {
     if ($('body').hasClass("template-product")) {
-        
+
         if($('.product__value-props li').length > 0){
-            //append values props to PDP product desc 
+            //append values props to PDP product desc
             $('.product__value-props').insertBefore('#accordion').removeClass('hide');
-            
+
             //spaced out value props if there are four or more values on a page
             if($('.product__value-props li').length >= 4){
                 $('.product__value-props').css("justify-content","space-between");
             }
         }
-        
+
         //variant pricing update
         // var variantID = $('.product-single__meta-worth').data('variant');
         var selectedVariant = '';
-       
+
         $(".selector-wrapper input[type=radio]").click(function() {
             selectedVariant = $(":checked").data('variant');
             $('.product-single__meta-worth').addClass('hide');
             $('.product-single__meta-worth').each(function( ) {
                 var variantID = $(this).data('variant');
-               
+
                 if(variantID == selectedVariant){
-                    
+
                     if($(this).hasClass('hide')){
                         $(this).removeClass('hide');
                     }
@@ -32,7 +32,7 @@ $(document).ready(function () {
             $('.product-single__meta-saving').addClass('hide');
             $('.product-single__meta-saving').each(function( ) {
                 var variantID = $(this).data('variant');
-                
+
                 if(variantID == selectedVariant){
                     if($(this).hasClass('hide')){
                         $(this).removeClass('hide');
@@ -44,25 +44,27 @@ $(document).ready(function () {
 
         //sticky-cart will slides up when reached end of main product div
          var heightThreshold_end  = $(".product-single").height();
-        
+
          $(window).scroll(function() {
              var scroll = $(window).scrollTop();
 
              if (scroll >=  heightThreshold_end ) {
                  $('.sticky-cart-wr').addClass('active');
+                 $('.sticky-cart-wr').removeClass('disable');
              } else {
                  $('.sticky-cart-wr').removeClass('active');
+                 $('.sticky-cart-wr').addClass('disable');
              }
          });
     }
 
-   
+
 
     //sticky addToCart
     $('#js-sticky-btn').click(function() {
         let variant = $(".selector-wrapper input[type=radio]:checked").data('variant');
         let qty = $('.js-product-form__item--quantity #Quantity').val();
-       
+
         data = {
             "id": variant,
             "quantity": qty,
@@ -79,14 +81,14 @@ $(document).ready(function () {
                     theme.miniCart.generateCart();
                     $('.js-mini-cart').removeClass('active').addClass('hide');
                     $('#jsCrosssell').addClass('show');
-                    
+
               });
             },
             complete: function () {
                 // $('.js-mini-cart').addClass('active');
             }
           });
-       
+
     });
 
     $('#jsCrosssell button.close').click(function() {
